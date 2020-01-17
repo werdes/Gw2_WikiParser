4f1072d48ac050027ea3890985b4004b911dbdd8
Unknown <marius_noack@yahoo.de>
2020-01-17 19:38:09 +0100

    init

---
+++ Gw2_WikiParser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gw2_WikiParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public Program()
+        {
+
+        }
+
+
+    }
+}
